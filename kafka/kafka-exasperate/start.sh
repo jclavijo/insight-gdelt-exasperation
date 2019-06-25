c9a8f07b535fbf7b742a40dc751be41e3aae7462
Juan-manuel Clavijo <clavijo.j@gmail.com>
2019-06-25 12:12:54 -0700

    Removed old project

--- kafka/kafka-exasperate/start.sh
+++
@@ -1,5 +0,0 @@
-# Start Zookeeper
-/usr/local/zookeeper/bin/zkServer.sh start
-# Start Kafka
-/usr/local/kafka/bin/kafka-server-start.sh -daemon /usr/local/kafka/config/server.properties
-# Create topic topic that the application consums from
